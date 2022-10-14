bf59ea47290a9a36b829bd8cebcf983a55d1f97a
d2gher <d2gher@gmail.com>
2022-10-14 11:56:13 +0200

    feat: added email view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -26,6 +26,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Show the mailbox name
   fetch(`/emails/${mailbox}`)
@@ -44,10 +45,10 @@ function load_mailbox(mailbox) {
           </div>
         `
         element.style.backgroundColor = email.read? "#ebebeb" : "White";
-      document.querySelector("#emails-view").append(element)
-      console.log(email)
+        element.addEventListener("click", () => email_view(email.id));
+      document.querySelector("#emails-view").append(element);
     });
-  })
+  }).catch(err => console.log(err))
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
 
@@ -73,4 +74,38 @@ compose_form.addEventListener('submit', function(event) {
   })
   .catch(function(error) { console.error(error) } )
 });
+
+function email_view(id) {
+  document.querySelector('#email-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  const email_box = document.querySelector("#email-view");
+
+  fetch(`/emails/${id}`)
+  .then(res => res.json())
+  .then(email => {
+    console.log(email)
+    email_box.innerHTML = 
+    `
+    <div class="card">
+      <h5 class="card-header">${email.subject}</h5>
+      <div class="card-body">
+        <h6 class="card-title">From: ${email.sender}</h6>
+        <h6 class="card-title">To: ${email.recipients}</h6>
+        <h6 class="card-title">At: ${email.timestamp}</h6>
+        <p class="card-text">${email.body}</p>
+        <a href="#" class="btn btn-primary">Replay</a>
+      </div>
+    </div>
+    `
+    fetch(`/emails/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        read: true
+      })
+    }).catch(err => console.log(err))
+  })
+
+}
 
